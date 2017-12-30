9bb1ceed23197080fcb8cd9db4973548d9df669e
Brian Azizi <briana@theodo.co.uk>
2017-12-30 19:42:11 +0000

    Create UserManager

--- backend/blog_api/apps/authentication/models.py
+++ backend/blog_api/apps/authentication/models.py
@@ -8,40 +8,36 @@ from django.db import models
 
 
 class UserManager(BaseUserManager):
-
-
-"""
-Django required that custom users define their own Manager class. By
-inheriting from `BaseUserManager`, we get a lot of the same code used 
-by Django to create a `User`.
-"""
-
-
-def create_user(self, username, email, password=None):
-    """create and return a `User` with an email, username and password."""
-    if username is None:
-        raise TypeError('Users must have a username.')
-
-    if email is None:
-        raise TypeError('Users must have an email address.')
-
-    user = self.model(username=username, email=self.normalize_email(email))
-    user.set_password(password)
-    user.save()
-
-    return user
-
-
-def create_superuser(self, username, email, password):
     """
-    Create and return a `User` with superuser (admin) permissions.
+    Django required that custom users define their own Manager class. By
+    inheriting from `BaseUserManager`, we get a lot of the same code used 
+    by Django to create a `User`.
     """
-    if password in None:
-        raise TypeError('Superusers must have a password.')
 
-    user = self.create_user(username, email, password)
-    user.is_superuser = True
-    user.is_staff = True
-    user.save()
+    def create_user(self, username, email, password=None):
+        """create and return a `User` with an email, username and password."""
+        if username is None:
+            raise TypeError('Users must have a username.')
 
-    return user
+        if email is None:
+            raise TypeError('Users must have an email address.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, username, email, password):
+        """
+        Create and return a `User` with superuser (admin) permissions.
+        """
+        if password is None:
+            raise TypeError('Superusers must have a password.')
+
+        user = self.create_user(username, email, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+
+        return user
