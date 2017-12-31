cb1e75749e6fd98537fbbe551b0d4d70eb413fb3
Brian Azizi <briana@theodo.co.uk>
2017-12-31 16:58:49 +0000

    Create Articles list endpoint

--- backend/blog_api/apps/articles/renderers.py
+++ backend/blog_api/apps/articles/renderers.py
@@ -3,3 +3,4 @@ from blog_api.apps.core.renderers import BlogAPIJSONRenderer
 
 class ArticleJSONRenderer(BlogAPIJSONRenderer):
     object_label = 'article'
+    object_label_plural = 'articles'
--- backend/blog_api/apps/core/renderers.py
+++ backend/blog_api/apps/core/renderers.py
@@ -1,21 +1,31 @@
 import json
 
 from rest_framework.renderers import JSONRenderer
+from rest_framework.utils.serializer_helpers import ReturnList
 
 
 class BlogAPIJSONRenderer(JSONRenderer):
     charset = 'utf-8'
     object_label = 'object'
+    object_label_plural = 'objects'
 
     def render(self, data, media_type=None, renderer_context=None):
-        # If the view throws an error, `data` will contain an `errors` key.
-        # We want the default JSONRenderer to handle rendering errors.
-        errors = data.get('errors', None)
+        if isinstance(data, ReturnList):
+            _data = json.loads(
+                super(BlogAPIJSONRenderer, self).render(data).decode('utf-8')
+            )
 
-        if errors is not None:
-            return super(BlogAPIJSONRenderer, self).render(data)
+            return json.dumps({
+                self.object_label_plural: _data
+            })
+        else:
+            # If the view throws an error, `data` will contain an `errors` key.
+            # We want the default JSONRenderer to handle rendering errors.
+            errors = data.get('errors', None)
 
-        # Finally, we can render our data under the "user" namespace.
-        return json.dumps({
-            self.object_label: data
-        })
+            if errors is not None:
+                return super(BlogAPIJSONRenderer, self).render(data)
+
+            return json.dumps({
+                self.object_label: data
+            })
