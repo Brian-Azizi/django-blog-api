a19b24c3779b8369931258eaf751cb390662d767
Brian Azizi <briana@theodo.co.uk>
2017-12-31 01:22:02 +0000

    Setup custom JWT authentication

--- backend/blog_api/settings.py
+++ backend/blog_api/settings.py
@@ -136,4 +136,7 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'EXCEPTION_HANDLER': 'blog_api.apps.core.exceptions.core_exception_handler',
     'NON_FIELD_ERRORS_KEY': 'error',
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'blog_api.apps.authentication.backends.JWTAuthentication',
+    ),
 }
