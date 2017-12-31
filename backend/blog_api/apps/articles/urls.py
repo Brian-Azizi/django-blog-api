841337c3f04b12080c7419dfbeedbcb3cfaab2fc
Brian Azizi <briana@theodo.co.uk>
2017-12-31 18:27:58 +0000

    Create comment destroy endpoint

--- backend/blog_api/apps/articles/urls.py
+++ backend/blog_api/apps/articles/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import include, url
 
 from rest_framework.routers import DefaultRouter
 
-from .views import ArticleViewSet, CommentsListCreateAPIView
+from .views import ArticleViewSet, CommentsListCreateAPIView, CommentsDestroyAPIView
 
 app_name = 'articles'
 
@@ -13,4 +13,6 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^articles/(?P<article_slug>[-\w]+)/comments/?$',
         CommentsListCreateAPIView.as_view()),
+    url(r'^articles/(?P<article_slug>[-\w]+)/comments/(?P<comment_pk>[\d]+)/?$',
+        CommentsDestroyAPIView.as_view()),
 ]
