8587903f19ebdb5c075dd4b77ac51cf8c373bfe5
Brian Azizi <briana@theodo.co.uk>
2017-12-31 00:52:25 +0000

    Render errors under `errors: error: ...`

--- backend/blog_api/apps/authentication/renderers.py
+++ backend/blog_api/apps/authentication/renderers.py
@@ -7,10 +7,17 @@ class UserJSONRenderer(JSONRenderer):
     charset = 'utf-8'
 
     def render(self, data, media_type=None, renderer_context=None):
+        # If the view throws an error, `data` will contain an `errors` key.
+        # We want the default JSONRenderer to handle rendering errors.
+        errors = data.get('errors', None)
+
         # If we receive a `token` key as part of the response, it will be a
         # byte object. Byte objects don't serialize well, so we need to
         # decode it before rendering the User object.
         token = data.get('token', None)
+
+        if errors is not None:
+            return super(UserJSONRenderer, self).render(data)
 
         if token is not None and isinstance(token, bytes):
             data['token'] = token.decode('utf-8')
