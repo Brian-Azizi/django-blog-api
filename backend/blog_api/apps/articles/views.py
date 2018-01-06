1ae9ac71aa9cec11feaa750473ab700dd5eb4549
Brian Azizi <briana@theodo.co.uk>
2018-01-06 22:38:18 +0000

    Add filtering

--- backend/blog_api/apps/articles/views.py
+++ backend/blog_api/apps/articles/views.py
@@ -25,6 +25,25 @@ class ArticleViewSet(mixins.CreateModelMixin,
     renderer_classes = (ArticleJSONRenderer,)
     serializer_class = ArticleSerializer
 
+    def get_queryset(self):
+        queryset = self.queryset
+
+        author = self.request.query_params.get('author', None)
+        if author is not None:
+            queryset = queryset.filter(author__user__username=author)
+
+        tag = self.request.query_params.get('tag', None)
+        if tag is not None:
+            queryset = queryset.filter(tags__tag=tag)
+
+        favorited_by = self.request.query_params.get('favorited', None)
+        if favorited_by is not None:
+            queryset = queryset.filter(
+                favorited_by__user__username=favorited_by
+            )
+
+        return queryset
+
     def create(self, request):
         serializer_context = {
             'author': request.user.profile,
@@ -42,7 +61,7 @@ class ArticleViewSet(mixins.CreateModelMixin,
 
     def list(self, request):
         serializer_context = {'request': request}
-        page = self.paginate_queryset(self.queryset)
+        page = self.paginate_queryset(self.get_queryset())
 
         serializer = self.serializer_class(
             page,
